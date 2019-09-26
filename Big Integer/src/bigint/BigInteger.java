f42640e7c882f53917c4b6568a13e008a3ede0f3
JScheire <jscheire08@gmail.com>
2019-09-25 21:27:38 -0400

    finished up functionality for adding positive BigIntegers in the add method

--- Big Integer/src/bigint/BigInteger.java
+++ Big Integer/src/bigint/BigInteger.java
@@ -1,9 +1,9 @@
 package bigint;
 
 /**
- * This class encapsulates a BigInteger, i.e. a positive or negative integer with 
- * any number of digits, which overcomes the computer storage length limitation of 
- * an integer.
+ * This class encapsulates a BigInteger, i.e. a positive or negative integer
+ * with any number of digits, which overcomes the computer storage length
+ * limitation of an integer.
  * 
  */
 public class BigInteger {
@@ -12,23 +12,22 @@ public class BigInteger {
 	 * True if this is a negative integer
 	 */
 	boolean negative;
-	
+
 	/**
 	 * Number of digits in this integer
 	 */
 	int numDigits;
-	
+
 	/**
 	 * Reference to the first node of this integer's linked list representation
 	 * NOTE: The linked list stores the Least Significant Digit in the FIRST node.
-	 * For instance, the integer 235 would be stored as:
-	 *    5 --> 3  --> 2
-	 *    
+	 * For instance, the integer 235 would be stored as: 5 --> 3 --> 2
+	 * 
 	 * Insignificant digits are not stored. So the integer 00235 will be stored as:
-	 *    5 --> 3 --> 2  (No zeros after the last 2)        
+	 * 5 --> 3 --> 2 (No zeros after the last 2)
 	 */
 	DigitNode front;
-	
+
 	/**
 	 * Initializes this integer to a positive number with zero digits, in other
 	 * words this is the 0 (zero) valued integer.
@@ -38,92 +37,80 @@ public class BigInteger {
 		numDigits = 0;
 		front = null;
 	}
-	
+
 	/**
-	 * Parses an input integer string into a corresponding BigInteger instance.
-	 * A correctly formatted integer would have an optional sign as the first 
+	 * Parses an input integer string into a corresponding BigInteger instance. A
+	 * correctly formatted integer would have an optional sign as the first
 	 * character (no sign means positive), and at least one digit character
-	 * (including zero). 
-	 * Examples of correct format, with corresponding values
-	 *      Format     Value
-	 *       +0            0
-	 *       -0            0
-	 *       +123        123
-	 *       1023       1023
-	 *       0012         12  
-	 *       0             0
-	 *       -123       -123
-	 *       -001         -1
-	 *       +000          0
-	 *       
-	 * Leading and trailing spaces are ignored. So "  +123  " will still parse 
+	 * (including zero). Examples of correct format, with corresponding values
+	 * Format Value +0 0 -0 0 +123 123 1023 1023 0012 12 0 0 -123 -123 -001 -1 +000
+	 * 0
+	 * 
+	 * Leading and trailing spaces are ignored. So " +123 " will still parse
 	 * correctly, as +123, after ignoring leading and trailing spaces in the input
 	 * string.
 	 * 
-	 * Spaces between digits are not ignored. So "12  345" will not parse as
-	 * an integer - the input is incorrectly formatted.
+	 * Spaces between digits are not ignored. So "12 345" will not parse as an
+	 * integer - the input is incorrectly formatted.
 	 * 
-	 * An integer with value 0 will correspond to a null (empty) list - see the BigInteger
-	 * constructor
+	 * An integer with value 0 will correspond to a null (empty) list - see the
+	 * BigInteger constructor
 	 * 
 	 * @param integer Integer string that is to be parsed
 	 * @return BigInteger instance that stores the input integer.
 	 * @throws IllegalArgumentException If input is incorrectly formatted
 	 */
-	public static BigInteger parse(String integer) 
-	throws IllegalArgumentException {
-		
-		//Check for empty string input
+	public static BigInteger parse(String integer) throws IllegalArgumentException {
+
+		// Check for empty string input
 		if ("".equals(integer)) {
 			throw new IllegalArgumentException();
 		}
-		
+
 		BigInteger intList = new BigInteger();
-		
-		//If integer has non integer values, throw exception
+
+		// If integer has non integer values, throw exception
 		try {
 			Integer.parseInt(integer);
-		}
-		catch(Exception e){
+		} catch (Exception e) {
 			throw new IllegalArgumentException();
 		}
-		
-		//Check if integer is negative or positive
-		if(integer.charAt(0) == '-') {
+
+		// Check if integer is negative or positive
+		if (integer.charAt(0) == '-') {
 			intList.negative = true;
-		}
-		else {
+		} else {
 			intList.negative = false;
 		}
-		
+
 		int zeroCounter = 0;
 		intList.front = null;
-		
-		//Count the leading zeros
+
+		// Count the leading zeros
 		while (zeroCounter < integer.length() && integer.charAt(zeroCounter) == '0') {
 			zeroCounter++;
 		}
-		
-		integer = integer.substring(zeroCounter,integer.length()); //If integer has leading zeros, delete them
-		
-		//Create the LinkedList
+
+		integer = integer.substring(zeroCounter, integer.length()); // If integer has leading zeros, delete them
+
+		// Create the LinkedList
 		for (int i = 0; i < integer.length(); i++) {
-			if(integer.charAt(i) != '+' && integer.charAt(i) != '-') {
+			if (integer.charAt(i) != '+' && integer.charAt(i) != '-') {
 				intList.front = new DigitNode(Character.getNumericValue((integer.charAt(i))), intList.front);
 				intList.numDigits++;
-				}
 			}
+		}
 		return intList;
 	}
-	
+
 	/**
-	 * Adds the first and second big integers, and returns the result in a NEW BigInteger object. 
-	 * DOES NOT MODIFY the input big integers.
+	 * Adds the first and second big integers, and returns the result in a NEW
+	 * BigInteger object. DOES NOT MODIFY the input big integers.
 	 * 
-	 * NOTE that either or both of the input big integers could be negative.
-	 * (Which means this method can effectively subtract as well.)
+	 * NOTE that either or both of the input big integers could be negative. (Which
+	 * means this method can effectively subtract as well.)
 	 * 
-	 * @param first First big integer
+	 * @param first  First big integer
 	 * @param second Second big integer
 	 * @return Result big integer
 	 */
@@ -131,65 +118,72 @@ public class BigInteger {
 		BigInteger result = new BigInteger();
 		DigitNode ptr1 = first.front;
 		DigitNode ptr2 = second.front;
+		DigitNode resultptr = result.front;
 		int carry = 0;
-		while(ptr1 != null && ptr2 != null) {
-				if(result.front == null) {			
-					if(ptr1.digit + ptr2.digit >= 10) {
-						result.front = new DigitNode((ptr1.digit + ptr2.digit) - 10, null);
-						carry++;
-					}
-					else {
-						result.front = new DigitNode(ptr1.digit + ptr2.digit + carry, null);
-						carry = 0;
-					}
+		while (ptr1 != null || ptr2 != null) {
+			if (result.front == null) {
+				if (ptr1.digit + ptr2.digit >= 10) {
+					result.front = new DigitNode((ptr1.digit + ptr2.digit) - 10, null);
+					resultptr = result.front;
+					carry++;
+				} else {
+					result.front = new DigitNode(ptr1.digit + ptr2.digit + carry, null);
+					resultptr = result.front;
+					carry = 0;
 				}
-				else{
-					DigitNode resultptr = result.front;
-					while(resultptr.next != null){
-						resultptr = resultptr.next;
-					}
-					if(ptr1.digit + ptr2.digit >= 10) {
+			} else {						
+				if (ptr1 == null) {
+					resultptr.next = new DigitNode(ptr2.digit + carry, null);
+					resultptr = resultptr.next;
+					carry = 0;
+				} else if (ptr2 == null) {
+					resultptr.next = new DigitNode(ptr1.digit + carry, null);
+					resultptr = resultptr.next;
+					carry = 0;
+				} else {
+					if (ptr1.digit + ptr2.digit >= 10) {
 						resultptr.next = new DigitNode((ptr1.digit + ptr2.digit) - 10, null);
+						resultptr = resultptr.next;
 						carry++;
-					}
-					else {
+					} else {
 						resultptr.next = new DigitNode(ptr1.digit + ptr2.digit + carry, null);
+						resultptr = resultptr.next;
 						carry = 0;
 					}
-						
-					}
-					ptr1 = ptr1.next;
-					ptr2 = ptr2.next;
 				}
-				
-		return result;
 			}
+			if(ptr1 != null) ptr1 = ptr1.next;
+			if(ptr2 != null) ptr2 = ptr2.next;
+		}
+		if (carry != 0) {
+			resultptr.next = new DigitNode(carry, null);
+		}
 
+		return result;
+	}
 
-			
-		
-		
-	
 	/**
-	 * Returns the BigInteger obtained by multiplying the first big integer
-	 * with the second big integer
+	 * Returns the BigInteger obtained by multiplying the first big integer with the
+	 * second big integer
 	 * 
 	 * This method DOES NOT MODIFY either of the input big integers
 	 * 
-	 * @param first First big integer
+	 * @param first  First big integer
 	 * @param second Second big integer
-	 * @return A new BigInteger which is the product of the first and second big integers
+	 * @return A new BigInteger which is the product of the first and second big
+	 *         integers
 	 */
 	public static BigInteger multiply(BigInteger first, BigInteger second) {
-		
+
 		/* IMPLEMENT THIS METHOD */
-		
+
 		// following line is a placeholder for compilation
 		return null;
 	}
-	
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	public String toString() {
@@ -198,9 +192,9 @@ public class BigInteger {
 		}
 		String retval = front.digit + "";
 		for (DigitNode curr = front.next; curr != null; curr = curr.next) {
-				retval = curr.digit + retval;
+			retval = curr.digit + retval;
 		}
-		
+
 		if (negative) {
 			retval = '-' + retval;
 		}
